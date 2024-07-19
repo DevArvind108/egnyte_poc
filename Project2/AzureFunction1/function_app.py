04ec6a393df10a79c105cd646611f7e14c563597
arrydotnet <arry.net@gmail.com>
2024-07-19 16:44:36 +0530

    Fuc tion app every 15 seconds

--- Project2/AzureFunction1/function_app.py
+++ Project2/AzureFunction1/function_app.py
@@ -5,14 +5,14 @@ import azure.functions as func
 app = func.FunctionApp()
 
 @app.function_name(name="mytimer")
-@app.schedule(schedule="01 0 0 * * *", 
+@app.schedule(schedule="*/15 * * * * *", #every 15 seconds
               arg_name="myTimer", 
               run_on_startup=True,
               use_monitor=False) 
 def asana_timer_trigger(myTimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
-    
+    logging.info("::-----------Python timer trigger 15 SEC---------::")
     if myTimer.past_due:
         logging.info('The timer is past due!')
 
